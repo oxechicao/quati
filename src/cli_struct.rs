286b3c849f0adbc6897ba6012cbd63476d86166b
Chicão Vieira <fthiagogv@gmail.com>
2025-12-11 11:23:01 -0300

    feat: 🚧 comment all old code

--- src/cli_struct.rs
+++ src/cli_struct.rs
@@ -1,44 +1,44 @@
-use clap::{Parser, Subcommand};
+// use clap::{Parser, Subcommand};
 
-/// CLI to manage git changes with AI assistance
-#[derive(Parser)]
-#[command(version, about, long_about = None)]
-pub struct Cli {
-    #[command(subcommand)]
-    pub action: Actions,
+// /// CLI to manage git changes with AI assistance
+// #[derive(Parser)]
+// #[command(version, about, long_about = None)]
+// pub struct Cli {
+//     #[command(subcommand)]
+//     pub action: Actions,
 
-    // Run in quiet mode
-    #[arg(short = 'q', long)]
-    pub quiet: Option<bool>,
-}
+//     // Run in quiet mode
+//     #[arg(short = 'q', long)]
+//     pub quiet: Option<bool>,
+// }
 
-/// Action to perform
-#[derive(Subcommand)]
-pub enum Actions {
-    /// Start a new branch locally and remotely
-    Start {
-        /// Name of the new branch
-        branch: Option<String>,
+// /// Action to perform
+// #[derive(Subcommand)]
+// pub enum Actions {
+//     /// Start a new branch locally and remotely
+//     Start {
+//         /// Name of the new branch
+//         branch: Option<String>,
 
-        /// Skip git hooks
-        #[arg(short = 'N', long = "no-verify", default_value_t = false)]
-        skip_hooks: bool,
+//         /// Skip git hooks
+//         #[arg(short = 'N', long = "no-verify", default_value_t = false)]
+//         skip_hooks: bool,
 
-        /// Remove the prefix from the branch name
-        #[arg(short = 'P', long = "no-prefix", default_value_t = false)]
-        remove_prefix: bool,
+//         /// Remove the prefix from the branch name
+//         #[arg(short = 'P', long = "no-prefix", default_value_t = false)]
+//         remove_prefix: bool,
 
-        /// Do not push the branch to origin
-        #[arg(short = 'S', long = "no-push", default_value_t = false)]
-        no_push: bool,
-    },
-}
+//         /// Do not push the branch to origin
+//         #[arg(short = 'S', long = "no-push", default_value_t = false)]
+//         no_push: bool,
+//     },
+// }
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn verify_cli() {
-        use clap::CommandFactory;
-        crate::cli_struct::Cli::command().debug_assert();
-    }
-}
+// #[cfg(test)]
+// mod tests {
+//     #[test]
+//     fn verify_cli() {
+//         use clap::CommandFactory;
+//         crate::cli_struct::Cli::command().debug_assert();
+//     }
+// }
