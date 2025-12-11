f5fc2883eb3096878f7489c87d7e16a0b4b44ec8
Chicão Vieira <fthiagogv@gmail.com>
2025-12-11 11:36:01 -0300

    feat: 🚧 criado teste para a função de branch_name com a utilização do mock
    Criada função get_current_branch_name utilizando generics para permitir o envio de um runner diferenciado.
    Criado o trait chamado CommandRunner, para ser utilizado como uma interface genérica para o runner da função.
    Criado a implementação do CommandRunner na struct de MockCommandRunner.

--- src/main.rs
+++ src/main.rs
@@ -32,15 +32,51 @@ fn main() {
 }
 
 
+#[derive(Clone,Debug)]
 pub struct RunResult {
     pub success: bool,
     pub stdout: Vec<u8>,
     pub stderr: Vec<u8>,
 }
 
+
+pub trait CommandRunner {
+    fn run(&mut self, program: &str, args: &[&str]) -> std::io::Result<RunResult>;
+}
+
 pub struct MockCommandRunner {
     pub result: RunResult,
 }
+
+impl CommandRunner for MockCommandRunner {
+    fn run(&mut self, _program: &str, _args: &[&str]) -> std::io::Result<RunResult> {
+        Ok(self.result.clone())
+    }
+}
+
+pub struct RealCommandRunner;
+impl CommandRunner for RealCommandRunner {
+    fn run(&mut self, program: &str, args: &[&str]) -> std::io::Result<RunResult> {
+        let output = Command::new(program).args(args).output()?;
+        Ok(RunResult {
+            success: output.status.success(),
+            stdout: output.stdout,
+            stderr: output.stderr,
+        })
+    }
+}
+
+pub fn get_current_branch_name<R: CommandRunner>(runner: &mut R) -> Result<String, String> {
+   let result = runner.run("git", &["rev-parse", "--abbrev-ref", "HEAD"])
+        .map_err(|e| format!("Failed: {}", e))?;
+
+    if result.success {
+        Ok(String::from_utf8_lossy(&result.stdout).trim().to_string())
+    } else {
+        Err(format!("Error: {}", String::from_utf8_lossy(&result.stderr)))
+    }
+}
+
 
 #[cfg(test)]
 mod tests {
@@ -60,5 +96,20 @@ mod tests {
             b"feature/test\n".to_vec()
         );
     }
+
+    #[test]
+    fn should_return_branch_name() {
+        let mut runner = MockCommandRunner {
+            result: RunResult {
+                success: true,
+                stdout: b"feature/test\n".to_vec(),
+                stderr: vec![],
+            },
+        };
+
+        let result = get_current_branch_name(&mut runner).unwrap();
+        assert_eq!(result, "feature/test");
+    }
+
 }
 
